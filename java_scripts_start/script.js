549e1e0300f7b2a172b4337afea39fe02b57688d
ViniciusMattoso <vinicius_matt@hotmail.com>
2022-07-02 22:36:45 -0300

    InitialCommit

---
+++ java_scripts_start/script.js
@@ -0,0 +1,18 @@
+var nome = "Vinicius";
+
+// Esse eh o comentario de uma linha
+
+/*
+Mas se quiser fazer um comentario maior
+pode usar esse jeito
+*/
+
+// Entrada de variaveis
+
+// var idade = prompt("Digite sua idade:");
+
+// Escrevendo na tela
+
+// document.write("<h1> Agora eu ja sei a sua idade </h1>");
+
+// document.write("<img src= 'https://www.tech4fan.com.br/2015/09/conheca-nova-logo-do-google-e-um-pouco-de-sua-historia.html'/>");
